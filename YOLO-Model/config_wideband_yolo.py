3c11f9021e925e5a6d4ae3d4380e33d955c65239
IainHigh <iain.high@sky.com>
2025-03-31 17:21:50 +0100

    48234948

--- YOLO-Model/config_wideband_yolo.py
+++ YOLO-Model/config_wideband_yolo.py
@@ -59,9 +59,9 @@ LEARNING_RATE = 0.001
 ########################
 # Loss Function Weights
 ########################
-LAMBDA_COORD = 1.0       # Weight for coordinate (x offset) loss
+LAMBDA_COORD = 15.0       # Weight for coordinate (x offset) loss
 LAMBDA_NOOBJ = 1.0       # Weight for confidence loss in no-object cells
-LAMBDA_CLASS = 5.0       # Weight for classification loss
+LAMBDA_CLASS = 1.0       # Weight for classification loss
     
 
 def print_config_file():
