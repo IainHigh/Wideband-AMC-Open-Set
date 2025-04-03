ab5ea07c267f081777bd3df7e391584bf61f163a
IainHigh <iain.high@sky.com>
2025-04-03 13:18:15 +0100

    48287571 - Trying with B=1 to force model to learn frequency error better

--- YOLO-Model/config_wideband_yolo.py
+++ YOLO-Model/config_wideband_yolo.py
@@ -45,8 +45,8 @@ NUMTAPS = 101 # Number of taps for the filter - Higher number of taps means bett
 #####################
 # Model Parameters
 #####################
-S = 8               # Number of grid cells
-B = 2                # Boxes per cell
+S = 4               # Number of grid cells
+B = 1                # Boxes per cell
 NUM_CLASSES = 9      # Number of classes
 
 #####################
